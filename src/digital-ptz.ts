0d1ff6eeef3e0517e7cc029faa73b46fd8cc0e0e
David Buezas <david.buezas@gmail.com>
2023-05-22 21:37:06 +0200

    Separate gestures from each other instead of keeping state

--- src/digital-ptz.ts
+++ src/digital-ptz.ts
@@ -1,157 +1,39 @@
 // js version generated from https://github.com/dbuezas/pan-zoom-controller/blob/main/src/digital-ptz.ts
 
-const MAX_ZOOM = 10;
-const LOCAL_STORAGE_KEY = "webrtc-digital-ptc:";
+import { Transform } from "./ditigal-ptz.transform";
+
 const ONE_FINGER_ZOOM_SPEED = 1 / 200; // 1 scale every 200px
-const ONE_FINGER_DOUBLE_TAP_ZOOM_MS_THRESHOLD = 250;
+const DBL_CLICK_MS = 250;
+const MAX_ZOOM = 10;
 
 const DEFAULT_OPTIONS = {
-  mouse_drag_zoom: true,
+  mouse_drag_pan: true,
   mouse_wheel_zoom: true,
   mouse_double_click_zoom: true,
-  touch_drag_zoom: true,
-  touch_pan: true,
+  touch_tap_drag_zoom: true,
+  touch_drag_pan: true,
   touch_pinch_zoom: true,
   touch_double_tap_zoom: true,
   persist_key: "",
   persist: true,
 };
 
-const STATE_IDLE = 0;
-const STATE_ONE_FINGER_PAN = 1;
-const STATE_ONE_FINGER_ZOOM = 2;
-const STATE_TWO_FINGERS = 3;
-
-const clamp = (value: number, min: number, max: number) =>
-  Math.min(Math.max(value, min), max);
-
-class Transform {
-  persist_key: string;
-  persist: boolean;
-  scale = 1;
-  x = 0;
-  y = 0;
-  videoRect?: DOMRect;
-  containerRect?: DOMRect;
-  constructor(persist_key: string, persist: boolean) {
-    this.persist = persist;
-    this.persist_key = LOCAL_STORAGE_KEY + persist_key;
-    this.loadPersistedTransform();
-  }
-  public updateRects(videoEl: HTMLVideoElement, containerEl: HTMLElement) {
-    this.containerRect = containerEl.getBoundingClientRect();
-    if (!videoEl.videoWidth) {
-      console.log("this.videoEl.videoWidth video not loaded");
-      return;
-    }
-
-    const screenAspectRatio =
-      this.containerRect.width / this.containerRect.height;
-    const videoAspectRatio = videoEl.videoWidth / videoEl.videoHeight;
-
-    if (videoAspectRatio > screenAspectRatio) {
-      // Black bars on the top and bottom
-      const videoHeight = this.containerRect.width / videoAspectRatio;
-      const blackBarHeight = (this.containerRect.height - videoHeight) / 2;
-      this.videoRect = new DOMRect(
-        this.containerRect.x,
-        blackBarHeight + this.containerRect.y,
-        this.containerRect.width,
-        videoHeight
-      );
-    } else {
-      // Black bars on the sides
-      const videoWidth = this.containerRect.height * videoAspectRatio;
-      const blackBarWidth = (this.containerRect.width - videoWidth) / 2;
-      this.videoRect = new DOMRect(
-        blackBarWidth + this.containerRect.x,
-        this.containerRect.y,
-        videoWidth,
-        this.containerRect.height
-      );
-    }
-  }
-
-  public move(dx: number, dy: number) {
-    if (!this.videoRect) return;
-    const bound = (this.scale - 1) / 2;
-    this.x += dx / this.videoRect.width;
-    this.y += dy / this.videoRect.height;
-    this.x = clamp(this.x, -bound, bound);
-    this.y = clamp(this.y, -bound, bound);
-    this.persistTransform();
-  }
-
-  // x,y are relative to viewport (clientX, clientY)
-  public zoomAtCoords(zoom: number, x: number, y: number) {
-    if (!this.containerRect || !this.videoRect) return;
-    const oldScale = this.scale;
-    this.scale *= zoom;
-    this.scale = clamp(this.scale, 1, MAX_ZOOM);
-    zoom = this.scale / oldScale;
-
-    x = x - this.containerRect.x - this.containerRect.width / 2;
-    y = y - this.containerRect.y - this.containerRect.height / 2;
-    const dx = x - this.x * this.videoRect.width;
-    const dy = y - this.y * this.videoRect.height;
-    this.move(dx * (1 - zoom), dy * (1 - zoom));
-  }
-
-  public zoom(zoom: number) {
-    if (!this.containerRect || !this.videoRect) return;
-    const x = this.containerRect.width / 2;
-    const y = this.containerRect.height / 2;
-    this.zoomAtCoords(zoom, x, y);
-  }
-
-  public render() {
-    if (!this.videoRect) return "";
-    const { x, y, scale } = this;
-    return `translate(${x * this.videoRect.width}px, ${
-      y * this.videoRect.height
-    }px) scale(${scale})`;
-  }
-
-  loadPersistedTransform = () => {
-    const { persist_key, persist } = this;
-    if (!persist) return;
-    try {
-      const loaded = JSON.parse(localStorage[persist_key]);
-      const isValid = [loaded.scale || loaded.x || loaded.y].every(
-        Number.isFinite
-      );
-      if (!isValid) {
-        throw new Error("Broken local storage");
-      }
-      this.x = loaded.x;
-      this.y = loaded.y;
-      this.scale = loaded.scale;
-    } catch (e) {
-      delete localStorage[persist_key];
-    }
-  };
-
-  persistTransform = () => {
-    if (!this.persist) return;
-    const { x, y, scale } = this;
-    localStorage[this.persist_key] = JSON.stringify({
-      x,
-      y,
-      scale,
-    });
-  };
-}
-
 type Options = {
-  mouse_drag_zoom?: boolean;
+  mouse_drag_pan?: boolean;
   mouse_wheel_zoom?: boolean;
   mouse_double_click_zoom?: boolean;
-  touch_drag_zoom?: boolean;
-  touch_pan?: boolean;
+  touch_tap_drag_zoom?: boolean;
+  touch_drag_pan?: boolean;
   touch_pinch_zoom?: boolean;
   touch_double_tap_zoom?: boolean;
   persist_key: string;
   persist: boolean;
+};
+
+const captureEvent = (e: Event) => {
+  e.preventDefault();
+  e.stopPropagation();
+  e.stopImmediatePropagation();
 };
 
 export class DigitalPTZ {
@@ -162,7 +44,6 @@ export class DigitalPTZ {
   videoEl: HTMLVideoElement;
   resizeObserver: ResizeObserver;
   transform: Transform;
-  state = STATE_IDLE;
   options: Options;
 
   constructor(
@@ -173,10 +54,11 @@ export class DigitalPTZ {
     this.containerEl = containerEl;
     this.videoEl = videoEl;
     this.options = Object.assign({}, DEFAULT_OPTIONS, options);
-    this.transform = new Transform(
-      this.options.persist_key,
-      this.options.persist
-    );
+    this.transform = new Transform({
+      persist_key: this.options.persist_key,
+      persist: this.options.persist,
+      maxZoom: MAX_ZOOM,
+    });
     for (const [event, handler] of this.handlers) {
       this.containerEl.addEventListener(event, handler as any, {
         capture: true,
@@ -201,179 +83,191 @@ export class DigitalPTZ {
     this.resizeObserver.unobserve(this.containerEl);
   }
 
-  private onTouchStart = async (e: TouchEvent) => {
-    const { touch_drag_zoom, touch_pan, touch_pinch_zoom } = this.options;
+  private startTouchTapDragZoom(downEvent: TouchEvent, isDoubleTap: boolean) {
+    const relevant = downEvent.touches.length === 1 && isDoubleTap;
+    captureEvent(downEvent);
+    if (!relevant) return false;
+    let lastTouchY = downEvent.touches[0].clientY;
+    const onTouchMove = (moveEvent: TouchEvent) => {
+      captureEvent(moveEvent);
+      const currTouchY = moveEvent.touches[0].clientY;
+      this.transform.zoom(
+        1 - (lastTouchY - currTouchY) * ONE_FINGER_ZOOM_SPEED
+      );
+      lastTouchY = currTouchY;
+      this.render();
+    };
 
-    let isValid = true;
-    const isQuickTap =
-      e.timeStamp - this.lastTap < ONE_FINGER_DOUBLE_TAP_ZOOM_MS_THRESHOLD;
+    this.containerEl.addEventListener("touchmove", onTouchMove);
+    const onTouchEnd = () =>
+      this.containerEl.removeEventListener("touchmove", onTouchMove);
+    this.containerEl.addEventListener("touchend", onTouchEnd, { once: true });
+    return true;
+  }
+  private startDoubleTapZoom(downEvent: TouchEvent, isDoubleTap: boolean) {
+    const relevant = downEvent.touches.length === 1 && isDoubleTap;
+    if (!relevant) return false;
+    this.containerEl.addEventListener(
+      "touchend",
+      (endEvent: TouchEvent) => {
+        captureEvent(endEvent);
+        const isQuickRelease = endEvent.timeStamp - this.lastTap < DBL_CLICK_MS;
+        if (!isQuickRelease) return;
+        if (this.transform.scale == 1) {
+          this.transform.zoomAtCoords(
+            2,
+            downEvent.touches[0].clientX,
+            downEvent.touches[0].clientY
+          );
+        } else {
+          this.transform.zoomAtCoords(0.01, 0, 0);
+        }
+        this.render();
+      },
+      { once: true }
+    );
+    captureEvent(downEvent);
+    return true;
+  }
 
-    if (e.touches.length === 1 && isQuickTap && touch_drag_zoom) {
-      this.state = STATE_ONE_FINGER_ZOOM;
-    } else if (e.touches.length === 1 && !isQuickTap && touch_pan) {
-      this.state = STATE_ONE_FINGER_PAN;
-    } else if (e.touches.length === 1 && !isQuickTap && touch_drag_zoom) {
-      // this also has to prevent default to avoid mousedown being triggered too.
-    } else if (e.touches.length === 2 && touch_pinch_zoom) {
-      this.state = STATE_TWO_FINGERS;
-    } else {
-      this.state = STATE_IDLE;
-      isValid = false;
-    }
-    this.lastTap = e.timeStamp;
+  private startOneFingerPan(downEvent: TouchEvent, isDoubleTap: boolean) {
+    const relevant = downEvent.touches.length === 1 && !isDoubleTap;
+    if (!relevant) return false;
 
-    if (isValid) {
-      e.preventDefault();
-      e.stopPropagation();
-      e.stopImmediatePropagation();
-      this.lastTouches = e.touches;
-    }
-  };
-
-  private onTouchMove = async (e: TouchEvent) => {
-    if (!this.lastTouches) return;
-
-    let isValid = true;
-    if (this.state === STATE_ONE_FINGER_PAN) {
-      const dx = e.touches[0].clientX - this.lastTouches[0].clientX;
-      const dy = e.touches[0].clientY - this.lastTouches[0].clientY;
+    let lastTouches = downEvent.touches;
+    const onTouchMove = (moveEvent: TouchEvent) => {
+      if (moveEvent.touches.length !== 1) return;
+      captureEvent(moveEvent);
+      const dx = moveEvent.touches[0].clientX - lastTouches[0].clientX;
+      const dy = moveEvent.touches[0].clientY - lastTouches[0].clientY;
       this.transform.move(dx, dy);
-    } else if (this.state === STATE_ONE_FINGER_ZOOM) {
-      const zoom =
-        1 -
-        (this.lastTouches[0].clientY - e.touches[0].clientY) *
-          ONE_FINGER_ZOOM_SPEED;
-      this.transform.zoom(zoom);
-    } else if (this.state === STATE_TWO_FINGERS) {
+      lastTouches = moveEvent.touches;
+      this.render();
+    };
+    this.containerEl.addEventListener("touchmove", onTouchMove);
+    const onTouchEnd = () =>
+      this.containerEl.removeEventListener("touchmove", onTouchMove);
+    this.containerEl.addEventListener("touchend", onTouchEnd, { once: true });
+    captureEvent(downEvent);
+    return true;
+  }
+  private startPinchZoom(downEvent: TouchEvent, isDoubleTap: boolean) {
+    const relevant = downEvent.touches.length === 2;
+    if (!relevant) return false;
+    captureEvent(downEvent);
+
+    let lastTouches = downEvent.touches;
+    const onTouchMove = (moveEvent: TouchEvent) => {
+      captureEvent(moveEvent);
       const oldCenter = {
-        x: (this.lastTouches[0].clientX + this.lastTouches[1].clientX) / 2,
-        y: (this.lastTouches[0].clientY + this.lastTouches[1].clientY) / 2,
+        x: (lastTouches[0].clientX + lastTouches[1].clientX) / 2,
+        y: (lastTouches[0].clientY + lastTouches[1].clientY) / 2,
       };
+      const newTouches = moveEvent.touches;
       const newCenter = {
-        x: (e.touches[0].clientX + e.touches[1].clientX) / 2,
-        y: (e.touches[0].clientY + e.touches[1].clientY) / 2,
+        x: (newTouches[0].clientX + newTouches[1].clientX) / 2,
+        y: (newTouches[0].clientY + newTouches[1].clientY) / 2,
       };
       const dx = newCenter.x - oldCenter.x;
       const dy = newCenter.y - oldCenter.y;
       const oldSpread = Math.hypot(
-        this.lastTouches[0].clientX - this.lastTouches[1].clientX,
-        this.lastTouches[0].clientY - this.lastTouches[1].clientY
+        lastTouches[0].clientX - lastTouches[1].clientX,
+        lastTouches[0].clientY - lastTouches[1].clientY
       );
       const newSpread = Math.hypot(
-        e.touches[0].clientX - e.touches[1].clientX,
-        e.touches[0].clientY - e.touches[1].clientY
+        newTouches[0].clientX - newTouches[1].clientX,
+        newTouches[0].clientY - newTouches[1].clientY
       );
       const zoom = newSpread / oldSpread;
       this.transform.move(dx, dy);
       this.transform.zoomAtCoords(zoom, newCenter.x, newCenter.y);
-    } else {
-      isValid = false;
-    }
-    if (isValid) {
-      e.preventDefault();
-      e.stopPropagation();
-      e.stopImmediatePropagation();
+      lastTouches = moveEvent.touches;
       this.render();
-      this.lastTouches = e.touches;
-    }
+    };
+    this.containerEl.addEventListener("touchmove", onTouchMove);
+    const onTouchEnd = () =>
+      this.containerEl.removeEventListener("touchmove", onTouchMove);
+    this.containerEl.addEventListener("touchend", onTouchEnd, { once: true });
+    return true;
+  }
+  private onTouchStart = (downEvent: TouchEvent) => {
+    const isDoubleTap = downEvent.timeStamp - this.lastTap < DBL_CLICK_MS;
+    this.lastTap = downEvent.timeStamp;
+
+    if (this.options.touch_tap_drag_zoom)
+      this.startTouchTapDragZoom(downEvent, isDoubleTap);
+    if (this.options.touch_drag_pan)
+      this.startOneFingerPan(downEvent, isDoubleTap);
+    if (this.options.touch_double_tap_zoom)
+      this.startDoubleTapZoom(downEvent, isDoubleTap);
+    if (this.options.touch_pinch_zoom)
+      this.startPinchZoom(downEvent, isDoubleTap);
   };
 
-  private onTouchEnd = (e: TouchEvent) => {
-    if (!this.lastTouches) return;
-    const { touch_double_tap_zoom } = this.options;
-    const isQuickTap =
-      e.timeStamp - this.lastTap < ONE_FINGER_DOUBLE_TAP_ZOOM_MS_THRESHOLD;
-
-    if (
-      this.state === STATE_ONE_FINGER_ZOOM &&
-      isQuickTap &&
-      touch_double_tap_zoom
-    ) {
-      if (this.transform.scale == 1) {
-        this.transform.zoomAtCoords(
-          2,
-          this.lastTouches[0].clientX,
-          this.lastTouches[0].clientY
-        );
-      } else {
-        this.transform.zoomAtCoords(0.01, 0, 0);
-      }
-      this.render();
-    }
-    this.state = STATE_IDLE;
-  };
-
-  private onWheel = async (e: WheelEvent) => {
+  private onWheel = (e: WheelEvent) => {
     if (!this.options.mouse_wheel_zoom) return;
+    captureEvent(e);
     const zoom = 1 - e.deltaY / 1000;
     this.transform.zoomAtCoords(zoom, e.clientX, e.clientY);
     this.render();
-    e.preventDefault();
-    e.stopPropagation();
-    e.stopImmediatePropagation();
   };
 
-  private onMouseDown = async (e: MouseEvent) => {
-    if (e.button !== 0) return;
-    const { mouse_double_click_zoom, mouse_drag_zoom } = this.options;
-    const isQuickTap =
-      e.timeStamp - this.lastTap < ONE_FINGER_DOUBLE_TAP_ZOOM_MS_THRESHOLD;
-    if (isQuickTap && mouse_double_click_zoom) {
-      this.state = STATE_ONE_FINGER_ZOOM;
-    } else if (mouse_drag_zoom) {
-      this.state = STATE_ONE_FINGER_PAN;
-    } else {
-      this.state = STATE_IDLE;
-    }
-    this.lastMouse = e;
-    this.lastTap = e.timeStamp;
-  };
+  private startDoubleClickZoom(downEvent: MouseEvent, isDoubleTap: boolean) {
+    if (!isDoubleTap) return false;
+    captureEvent(downEvent);
+    this.containerEl.addEventListener(
+      "mouseup",
+      (upEvent: MouseEvent) => {
+        captureEvent(upEvent);
+        const isQuickRelease = upEvent.timeStamp - this.lastTap < DBL_CLICK_MS;
+        if (!isQuickRelease) return;
+        if (this.transform.scale == 1) {
+          this.transform.zoomAtCoords(2, downEvent.clientX, downEvent.clientY);
+        } else {
+          this.transform.zoomAtCoords(0.01, 0, 0);
+        }
+        this.render();
+      },
+      { once: true }
+    );
+    return true;
+  }
 
-  private onMouseMove = async (e: MouseEvent) => {
-    if (!this.lastMouse) return;
-    if (this.state === STATE_ONE_FINGER_PAN) {
-      const dx = e.x - this.lastMouse.x;
-      const dy = e.y - this.lastMouse.y;
+  private startMouseDragPan(downEvent: MouseEvent, isDoubleTap: boolean) {
+    if (isDoubleTap) return false;
+    captureEvent(downEvent);
+    let lastMouse = downEvent;
+    const onMouseMove = (moveEvent: MouseEvent) => {
+      captureEvent(moveEvent);
+      const dx = moveEvent.x - lastMouse.x;
+      const dy = moveEvent.y - lastMouse.y;
       this.transform.move(dx, dy);
+      lastMouse = moveEvent;
       this.render();
-      e.preventDefault();
-      e.stopPropagation();
-      e.stopImmediatePropagation();
-      this.lastMouse = e;
-    }
+    };
+    this.containerEl.addEventListener("mousemove", onMouseMove);
+    this.containerEl.addEventListener(
+      "mouseup",
+      () => this.containerEl.removeEventListener("mousemove", onMouseMove),
+      { once: true }
+    );
+    return true;
+  }
+
+  private onMouseDown = (downEvent: MouseEvent) => {
+    const isDoubleTap = downEvent.timeStamp - this.lastTap < DBL_CLICK_MS;
+    this.lastTap = downEvent.timeStamp;
+
+    if (this.options.mouse_double_click_zoom)
+      this.startDoubleClickZoom(downEvent, isDoubleTap);
+    if (this.options.mouse_drag_pan)
+      this.startMouseDragPan(downEvent, isDoubleTap);
   };
 
-  private onMouseUp = (e: MouseEvent) => {
-    if (!this.lastMouse) return;
-    const { mouse_double_click_zoom } = this.options;
-    const isQuickTap =
-      e.timeStamp - this.lastTap < ONE_FINGER_DOUBLE_TAP_ZOOM_MS_THRESHOLD;
-    if (
-      this.state === STATE_ONE_FINGER_ZOOM &&
-      isQuickTap &&
-      mouse_double_click_zoom
-    ) {
-      if (this.transform.scale == 1) {
-        this.transform.zoomAtCoords(
-          2,
-          this.lastMouse.clientX,
-          this.lastMouse.clientY
-        );
-      } else {
-        this.transform.zoomAtCoords(0.01, 0, 0);
-      }
-      this.render();
-    }
-    this.state = STATE_IDLE;
-  };
   private handlers = [
     ["wheel", this.onWheel],
     ["touchstart", this.onTouchStart],
-    ["touchmove", this.onTouchMove],
-    ["touchend", this.onTouchEnd],
     ["mousedown", this.onMouseDown],
-    ["mousemove", this.onMouseMove],
-    ["mouseup", this.onMouseUp],
   ] as const;
 
   private render() {
